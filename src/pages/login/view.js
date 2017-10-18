d93c899206c7db287d93ba517d12b3f39a20d4c3
Jef Braem <jef_braem@hotmail.com>
2017-10-18 13:39:50 +0200

    mobx-react-form implemented on view.js

--- src/pages/login/view.js
+++ src/pages/login/view.js
@@ -7,82 +7,95 @@ import { colors } from '../../constants/colors';
 
 const logo = require('./smackLogo.svg');
 
+//FORMS
+import validatorjs from 'validatorjs';
+import MobxReactForm from 'mobx-react-form';
+import { observer } from 'mobx-react';
+
+const fields = [{
+  name: 'username',
+  label: 'Username',
+  type: 'text',
+  placeholder: 'Your name',
+  rules: 'required|string',
+}];
+
+const plugins = { dvr: validatorjs };
+
+const hooks = {
+  onSuccess(form) {
+    alert('Form is valid! Send the request here.');
+    // get field values
+    console.log('Form Values!', form.values());
+  },
+  onError(form) {
+    // get all form errors
+    console.log('All form errors', form.errors());
+    // invalidate the form with a custom error message
+    form.invalidate('This is a generic error message!');
+  },
+};
+
+const form = new MobxReactForm({ fields }, { plugins, hooks });
+
 const LoginDiv = styled.div`
-align-items: center;
-background-color: ${colors.darkPurple};
-display: flex;
-height: 100%;
-justify-content: center;
-position: absolute;
-width: 100%;
+  align-items: center;
+  background-color: ${colors.darkPurple};
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
 `;
 
 const SmackForm = styled.form`
-height: 250px;
-width: 212px;
+  height: 250px;
+  width: 212px;
 `;
 
 const LoginLogo = styled.img`
-margin-bottom: 29px;
+  margin-bottom: 29px;
 `;
 
 const LoginInput = styled.input`
-background-color: ${colors.white};
-border: solid 1px ${colors.grey};
-border-radius: 6px;
-box-shadow: inset 0 1px 3px 0 ${colors.lightGrey};
-color: ${colors.silver};
-font-family: 'Rubik-Italic';
-font-size: 16px;
-height: 40px;
-margin-bottom: 11px;
-text-align: center;
-width: 212px;
+  background-color: ${colors.white};
+  border: solid 1px ${colors.grey};
+  border-radius: 6px;
+  box-shadow: inset 0 1px 3px 0 ${colors.lightGrey};
+  color: ${colors.silver};
+  font-family: 'Rubik-Italic';
+  font-size: 16px;
+  height: 40px;
+  margin-bottom: 11px;
+  text-align: center;
+  width: 212px;
 `;
 
 const LoginButton = styled.button`
-background-color: ${colors.darkPurple};
-border: solid 1px ${colors.lightGrey};
-border-radius: 6px;
-color: ${colors.white};
-font-family: 'Rubik-Light';
-height: 40px;
-width: 212px;
+  background-color: ${colors.darkPurple};
+  border: solid 1px ${colors.lightGrey};
+  border-radius: 6px;
+  color: ${colors.white};
+  font-family: 'Rubik-Light';
+  height: 40px;
+  width: 212px;
 `;
 
-@reduxForm({
-  form: 'login',
-  fields: [ 'username' ],
-    // Get the form state. // TODO: fix this bug
-  getFormState: (state, reduxMountPoint) => state.get(reduxMountPoint)
-})
+@observer
 export default class LoginForm extends Component {
-
-  static propTypes = {
-    fields: PropTypes.object.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    resetForm: PropTypes.func.isRequired,
-    submitting: PropTypes.bool.isRequired
-  };
 
   constructor (props) {
     super(props);
-    this.submit = ::this.submit;
-  }
-
-  submit (values, dispatch) {
-    dispatch(submit(values));
   }
 
   render () {
-    const { fields: { username }, handleSubmit, submitting } = this.props;
 
     return (
       <LoginDiv>
-        <SmackForm onSubmit={handleSubmit(this.submit)}>
+        <SmackForm onSubmit={form.onSubmit}>
           <LoginLogo src={logo} />
-          <LoginInput {...username} autoComplete='off' placeholder='Your name' type='text' />
-          <LoginButton disabled={submitting} type='submit'>Join</LoginButton>
+          <LoginInput {...form.$('username').bind()}/>
+          <LoginButton type='submit'>Join</LoginButton>
         </SmackForm>
       </LoginDiv>
     );
